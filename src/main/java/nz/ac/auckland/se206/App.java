760c69b5a1d480c6417460fd348871d271e1f326
Jerry Luo <jluo201@aucklanduni.ac.nz>
2025-09-12 17:48:30 +1200

    Added verdict scene to SceneManager

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -106,6 +106,7 @@ public class App extends Application {
     SceneManager.addUi(AppUi.defendantMemory, loadFxml("defendantMemory"));
     SceneManager.addUi(AppUi.humanMemory, loadFxml("humanMemory"));
     SceneManager.addUi(AppUi.aiMemory, loadFxml("aiMemory"));
+    SceneManager.addUi(AppUi.verdict, loadFxml("verdict"));
     Parent root = SceneManager.getUiRoot(AppUi.room);
 
     SceneManager.initializeChats();
