bc94472616129e8f50a8b815ddf9168efd5cc4ab
_ProtonPLUS <mcib088@aucklanduni.ac.nz>
2025-09-23 15:55:37 +1200

    Updated the timer, so that the text is updated immediately

--- src/main/java/nz/ac/auckland/se206/utils/Timer.java
+++ src/main/java/nz/ac/auckland/se206/utils/Timer.java
@@ -46,7 +46,16 @@ public class Timer {
    */
   public Timer addConsumer(Consumer<String> consumer) {
     this.consumers.add(consumer);
-    updateTimerLabel(this.count);
+
+    double minutes = Math.floor(this.count / 60.0);
+    double seconds = this.count % 60; // Remainder
+
+    StringBuilder builder = new StringBuilder();
+    builder.append(String.format("%02.0f", minutes));
+    builder.append(" : ");
+    builder.append(String.format("%02.0f", seconds));
+
+    consumer.accept(builder.toString());
     return this;
   }
 
