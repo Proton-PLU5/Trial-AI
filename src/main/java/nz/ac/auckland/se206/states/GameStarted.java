4a528e7cf88a7b30aba4336d0bdb47ea05c1f728
shabebe11 <salk147@aucklanduni.ac.nz>
2025-09-26 15:01:47 +1200

    Changing more of the code to match the codestyle stuff

--- src/main/java/nz/ac/auckland/se206/states/GameStarted.java
+++ src/main/java/nz/ac/auckland/se206/states/GameStarted.java
@@ -1,13 +1,14 @@
 package nz.ac.auckland.se206.states;
 
 import java.io.IOException;
+
 import javafx.scene.input.MouseEvent;
-import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameStateContext;
 import nz.ac.auckland.se206.speech.TextToSpeech;
 
 /**
- * The GameStarted state of the game. Handles the initial interactions when the game starts,
+ * The GameStarted state of the game. Handles the initial interactions when the
+ * game starts,
  * allowing the player to chat with characters and prepare to make a guess.
  */
 public class GameStarted implements GameState {
@@ -24,16 +25,18 @@ public class GameStarted implements GameState {
   }
 
   /**
-   * Handles the event when a rectangle is clicked. Depending on the clicked rectangle, it either
+   * Handles the event when a rectangle is clicked. Depending on the clicked
+   * rectangle, it either
    * provides an introduction or transitions to the chat view.
    *
-   * @param event the mouse event triggered by clicking a rectangle
+   * @param event       the mouse event triggered by clicking a rectangle
    * @param rectangleId the ID of the clicked rectangle
    * @throws IOException if there is an I/O error
    */
   @Override
   public void handleRectangleClick(MouseEvent event, String rectangleId) throws IOException {
-    // Transition to chat view or provide an introduction based on the clicked rectangle
+    // Transition to chat view or provide an introduction based on the clicked
+    // rectangle
     switch (rectangleId) {
       case "rectCashier":
         TextToSpeech.speak("Welcome to my cafe!");
@@ -42,11 +45,12 @@ public class GameStarted implements GameState {
         TextToSpeech.speak("Hi, let me know when you are ready to order!");
         return;
     }
-    // App.openChat(event, context.getProfession(rectangleId));
+
   }
 
   /**
-   * Handles the event when the guess button is clicked. Prompts the player to make a guess and
+   * Handles the event when the guess button is clicked. Prompts the player to
+   * make a guess and
    * transitions to the guessing state.
    *
    * @throws IOException if there is an I/O error
