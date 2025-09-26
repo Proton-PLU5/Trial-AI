9450580b0584d85322b421179ee4c551c815a255
_ProtonPLUS <mcib088@aucklanduni.ac.nz>
2025-09-26 19:58:31 +1200

    Fixed the unused imports error on the code style

--- src/main/java/nz/ac/auckland/se206/controllers/memory/MemoryController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/memory/MemoryController.java
@@ -10,7 +10,6 @@ import java.nio.file.Paths;
 import java.util.List;
 import javafx.animation.Interpolator;
 import javafx.animation.PauseTransition;
-import javafx.animation.ScaleTransition;
 import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
